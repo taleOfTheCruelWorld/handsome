d9e16e26aad769868c975d6e6e286f24526e19fa
taleOfTheCruelWorld <gomonov638@gmail.com>
2026-02-07 11:09:17 +0700

    db fix

--- database/migrations/2026_02_07_033846_tables.php
+++ database/migrations/2026_02_07_033846_tables.php
@@ -10,24 +10,14 @@ return new class extends Migration {
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->id();
-            $table->string('login');
-            $table->string('password');
-            $table->timestamp('created_at');
+        Schema::table('users', function (Blueprint $table) {
+            $table->renameColumn('name', 'login');
         });
 
-        Schema::create('orders', function (Blueprint $table) {
+
+        Schema::create('cities', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('phone');
-            $table->foreignId('city_id')->constrained();
-            $table->string('adress');
-            $table->string('size');
-            $table->foreignId('type_id')->constrained();
-            $table->foreignId('color_id')->constrained();
-            $table->foreignId('retake_type_id')->constrained();
-            $table->timestamp('created_at');
         });
 
         Schema::create('types', function (Blueprint $table) {
@@ -44,6 +34,21 @@ return new class extends Migration {
             $table->id();
             $table->string('name');
         });
+
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('phone');
+            $table->foreignId('city_id')->constrained();
+            $table->string('adress');
+            $table->string('size');
+            $table->foreignId('type_id')->constrained();
+            $table->foreignId('color_id')->constrained();
+            $table->foreignId('retake_type_id')->constrained();
+            $table->timestamp('created_at');
+        });
+
+
     }
 
     /**
@@ -51,6 +56,10 @@ return new class extends Migration {
      */
     public function down(): void
     {
-        //
+        Schema::dropIfExists('cities');
+        Schema::dropIfExists('types');
+        Schema::dropIfExists('colors');
+        Schema::dropIfExists('retake_types');
+        Schema::dropIfExists('orders');
     }
 };
